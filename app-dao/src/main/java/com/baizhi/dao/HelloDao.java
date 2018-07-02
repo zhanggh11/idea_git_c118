7a75720d6ad87d8914fc7a4ce4ed49d07b7b9aeb
zhanggh <1021770235@qq.com>
2018-07-02 22:19:35 +0800

    第一次提交，项目基础结构和基础代码

---
+++ app-dao/src/main/java/com/baizhi/dao/HelloDao.java
@@ -0,0 +1,13 @@
+package com.baizhi.dao;
+
+/**
+ * Created by Administrator on 2018/7/1.
+ */
+public class HelloDao {
+
+    public String sayHello(String name){
+
+        return "name :" + name;
+    }
+
+}
